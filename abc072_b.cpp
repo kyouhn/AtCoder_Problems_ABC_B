086df2ef5a9a2a2ff140376a520bd0e1f69eea68
kyouhn <kzk0802kazuki@gmail.com>
2019-01-25 23:29:56 +0900

    ABC072のB問題できた

---
+++ abc072_b.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	string ans;
+	for (int i = 0; i < s.size(); i+=2) {
+		ans += s[i];
+	}
+
+	cout << ans << endl;
+	return 0;
+}
