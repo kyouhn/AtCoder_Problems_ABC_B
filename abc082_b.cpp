d2f221b7988342532a8d3cb5e2c044f7fc343d82
kyouhn <kzk0802kazuki@gmail.com>
2019-01-25 21:30:49 +0900

    ABC082のB問題できた

---
+++ abc082_b.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	sort(s.begin(), s.end());
+
+	string t;
+	cin >> t;
+	sort(t.begin(), t.end());
+	reverse(t.begin(), t.end());
+
+	if (s < t) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+
+	return 0;
+}
